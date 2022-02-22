aa74b6e69567c89efa69c38d036dfedb45f1aed8
Eliton <elitonfilho@gmail.com>
2022-02-22 00:17:20 -0300

    feat: blog's initial page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,35 @@
 module.exports = {
-    siteMetadata: {
-      title: `Eliton's Blog`,
-      author: {
-          name: `Eliton`,
-          summary: `A guy.`,
-      },
-      description: `Trying to write useful and not boring stuff`,
+  siteMetadata: {
+    title: `Eliton's Blog`,
+    author: {
+      name: `Eliton`,
+      summary: `A guy.`,
     },
-    plugins: [
-      {
-        resolve: `gatsby-plugin-mdx`,
-        options: {
-          extensions: [`.mdx`, `.md`],
-        },
+    description: `Trying to write useful and not boring stuff`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [],
       },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/blog`,
-          name: `blog`,
-        },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
       },
-    ],
-  };
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: `images`,
+      },
+    },
+  ],
+};
