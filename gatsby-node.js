7eb98ce6679a23182bf7691d37fe1a416aae6b89
Eliton <elitonfilho@gmail.com>
2022-07-28 18:58:14 +0200

    fix: graphql shema def

--- gatsby-node.js
+++ gatsby-node.js
@@ -126,7 +126,7 @@ exports.createSchemaCustomization = ({ actions }) => {
       title: String
       description: String
       date: Date @dateformat
-      tags: Array
+      tags: [String]
     }
 
     type Fields {
