d355903e9bc6612715458e2fb01715fd238dae02
Eliton <elitonfilho@gmail.com>
2022-07-28 19:20:03 +0200

    fix: null tags on build phase

--- src/components/tags.js
+++ src/components/tags.js
@@ -4,7 +4,7 @@ import * as React from "react"
 const TagsBlogPost = ({ inArticle, tags }) => (
         <div inArticle={inArticle} className="tags-article">
             <Link to="/tags/">Tags:</Link>
-            {tags.map(tag =>
+            {tags && tags.map(tag =>
                 <Link to={`/tags/${tag}`}>{tag}</Link>
             )}
         </div>
