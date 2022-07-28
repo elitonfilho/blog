32c4f2dfb9bccbb83ed97d6201859b2711b66bd3
Eliton <elitonfilho@gmail.com>
2022-07-28 15:31:56 +0200

    chore: style on project comp

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import * as React from "react"
 
 const ProjectComponent = () => {
-    const queryData = useStaticQuery(graphql`
+  const queryData = useStaticQuery(graphql`
     query ProjectQuery {
         site {
           siteMetadata {
@@ -22,31 +22,39 @@ const ProjectComponent = () => {
         }
     }`)
 
-    const personalProjects = queryData.site.siteMetadata.projects?.personal
-    const workProjects = queryData.site.siteMetadata.projects?.work
+  const personalProjects = queryData.site.siteMetadata.projects?.personal
+  const workProjects = queryData.site.siteMetadata.projects?.work
 
-    return (
-        <div>
-            <h5 style={{ textAlign: `center` }}>Here are some personal projects that i've worked on: </h5>
-            <ol>{
-                personalProjects.map(element => {
-                    return (
-                        <li key={element.name}>
-                            <a href={element.link}>{element.name}</a>
-                            <p>{element.description}</p>
-                        </li>)
-                })}</ol>
-            <h5 style={{ textAlign: `center` }}>And some open-source work projects that i've worked with: </h5>
-            <ol>{
-                workProjects.map(element => {
-                    return (
-                        <li key={element.name}>
-                            <a href={element.link}>{element.name}</a>
-                            <p>{element.description}</p>
-                        </li>)
-                })}</ol>
-        </div>
-    )
+  return (
+    <div>
+      <h5 style={{ textAlign: `center` }}>Here are some personal projects that i've worked on: </h5>
+      <ol>{
+        personalProjects.map(element => {
+          return (
+            <li key={element.name}>
+              <a href={element.link} style={{ color: `var(--color-text)` }}>
+                <strong>
+                  {element.name}
+                </strong>
+              </a>
+              <p>{element.description}</p>
+            </li>)
+        })}</ol>
+      <h5 style={{ textAlign: `center` }}>And some open-source work projects that i've worked with: </h5>
+      <ol>{
+        workProjects.map(element => {
+          return (
+            <li key={element.name} >
+              <a href={element.link} style={{ color: `var(--color-text)` }}>
+                <strong>
+                  {element.name}
+                </strong>
+              </a>
+              <p>{element.description}</p>
+            </li>)
+        })}</ol>
+    </div>
+  )
 }
 
 export default ProjectComponent
